432857e05bb31000a4e74ccdb9161bb3c62dae96
itsmefreak <itsmefreak@tuta.io>
2022-03-20 21:47:38 +0100

    Max Value[] of Integer

---
+++ Practice/src/basics/FindMaximum.java
@@ -0,0 +1,26 @@
+package basics;
+
+import java.util.Arrays;
+
+public class FindMaximum {
+
+//Find Maximum
+//Write a method that returns the largest integer in the list.
+//You can assume that the list has at least one element.
+
+    public static Integer maximum(Integer[] list){
+        int result = 1;
+        for (int i=0; i<list.length; i++){
+            if (list[i] > result) {
+                result = list[i];
+            };
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = {3,16,5,1,8,15,17};
+        System.out.println(maximum(list));
+    }
+
+}
